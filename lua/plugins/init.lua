77b90cf8c4944ad2a2bbf0e8ba9900aa7c572f56
Brett Tanner <bretttanner171@gmail.com>
2024-03-30 14:16:15 +0900

    Setup neotest

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -35,6 +35,9 @@ return {
 			"nvim-lua/plenary.nvim",
 			"antoinemadec/FixCursorHold.nvim",
 			"nvim-treesitter/nvim-treesitter",
+			"nvim-neotest/neotest-go",
+			"rouge8/neotest-rust",
 		},
 	},
+	{ "folke/neodev.nvim", opts = {} },
 }
