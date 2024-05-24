95d0a7b56bfce95dff0b497da9b90fd29d07fb22
Brett <bretttanner171@gmail.com>
2024-05-24 11:41:19 +0900

    Turn off debug mode

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -105,7 +105,6 @@ local null_ls = require("null-ls")
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
-	debug = true,
 	ensure_installed = {
 		"gofumpt",
 		"goimports",
