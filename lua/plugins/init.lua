efee3b65087204cdea1bd66465fbee20f2506eae
Brett <bretttanner171@gmail.com>
2024-06-03 11:32:59 +0900

    Switch to lazydev

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -22,7 +22,16 @@ return {
 	{ "L3MON4D3/LuaSnip" },
 	{ "VonHeikemen/lsp-zero.nvim", branch = "v3.x" },
 	{ "hrsh7th/cmp-nvim-lsp" },
-	{ "hrsh7th/nvim-cmp" },
+	{
+		"hrsh7th/nvim-cmp",
+		opts = function(_, opts)
+			opts.sources = opts.sources or {}
+			table.insert(opts.sources, {
+				name = "lazydev",
+				group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+			})
+		end,
+	},
 	{ "neovim/nvim-lspconfig" },
 	{ "nvim-telescope/telescope.nvim" },
 	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
@@ -39,7 +48,6 @@ return {
 			"rouge8/neotest-rust",
 		},
 	},
-	{ "folke/neodev.nvim", opts = {} },
 	{
 		"nvim-lualine/lualine.nvim",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
@@ -52,4 +60,19 @@ return {
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 	},
 	{ "folke/which-key.nvim", event = "VimEnter" },
+	{
+		"folke/lazydev.nvim",
+		ft = "lua", -- only load on lua files
+		opts = {
+			library = {
+				-- Library items can be absolute paths
+				-- "~/projects/my-awesome-lib",
+				-- Or relative, which means they will be resolved as a plugin
+				-- "LazyVim",
+				-- When relative, you can also provide a path to the library in the plugin dir
+				"luvit-meta/library", -- see below
+			},
+		},
+	},
+	{ "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
 }
