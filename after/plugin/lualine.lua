c18ba80991308ee8db728a6f0ea17afccc1d3b8f
Brett Tanner <bretttanner171@gmail.com>
2024-06-15 09:18:56 +0900

    Lualine customisations

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,5 +1,24 @@
 require("lualine").setup({
+	extensions = { "oil" },
 	options = {
+		globalstatus = true,
 		theme = "auto",
+	},
+	winbar = {
+		lualine_a = { { "filename", path = 1 } },
+		lualine_b = {},
+		lualine_c = {},
+		lualine_x = {},
+		lualine_y = {},
+		lualine_z = {},
+	},
+
+	inactive_winbar = {
+		lualine_a = { { "filename", path = 1 } },
+		lualine_b = {},
+		lualine_c = {},
+		lualine_x = {},
+		lualine_y = {},
+		lualine_z = {},
 	},
 })
